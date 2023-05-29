cb9a9d11823cad750c99ace7cc5f29c054b224b9
GuilhermeSgithub <guiilhermesanttosgithub@gmail.com>
2023-05-29 18:18:51 -0300

    banco

--- banco.kt
+++ banco.kt
@@ -2,10 +2,26 @@ data class Cliente(val nome: String, val senha: String, var saldo: Double)
 
 var saldoLogado: Double = 0.00
 var usuarioLogado: String = ""
-val extratoSaque = mutableListOf<String>()
-val extratoDeposito = mutableListOf<String>()
+val extratoSaque = mutableListOf<Double>()
+val extratoDeposito = mutableListOf<Double>()
 
 val listaClientes = mutableListOf<Cliente>()
+
+
+fun entradaValor(valor: Any): Double {
+    val valorSaida = valor.toString()
+
+    if (valor == String) {
+        return 0.0
+    } else if (valor == "") {
+        return 0.0
+    } else if (valor == " ") {
+        return 0.0
+    } else {
+        return valorSaida.toDouble()
+    }
+}
+
 
 fun login(saldoAtual: Double = 0.00): Boolean {
     println("digite seu login")
@@ -28,6 +44,7 @@ fun main() {
     listaClientes.add(Cliente("matheus", "1234", 200.00))
     listaClientes.add(Cliente("leonardo", "1832", 8000.00))
 
+
     while (true) {
         var logado = login()
 
@@ -44,40 +61,43 @@ fun main() {
     while (true) {
         println("1-Sacar 2-Depositar 3-Extrato 4-Sair")
 
-        var opçao = readlnOrNull().toString()
+        val opção = readLine()?.toString()
 
-        if (opçao == "1") {
-            println("quanto quer sacar ?")
-            var valor = readlnOrNull()?.toInt()
-            if (saldoLogado <= valor!!) {
-                println("saldo insuficiente para saque")
-                println("voltando ao menu")
-            } else {
-                saldoLogado = saldoLogado - valor!!
-                println("Contando notas ")
-                println("Retire seu dinheiro abaixo")
-                extratoSaque.add(valor.toString())
-                println("voltando ao menu")
-                true
+        when (opção) {
+            "1" -> {
+                println("quanto quer sacar ?")
+                val valor = entradaValor(readLine()?.toDoubleOrNull() ?: 0.0)
+                if (saldoLogado <= valor) {
+                    println("saldo insuficiente para saque")
+                    println("voltando ao menu")
+                } else {
+                    saldoLogado -= valor
+                    println("Contando notas ")
+                    println("Retire seu dinheiro abaixo")
+                    extratoSaque.add(valor)
+                    println("voltando ao menu")
+                }
             }
-        } else if (opçao == "2") {
-            println("quanto quer depositar ?")
-            var valor = readlnOrNull()?.toInt()
-            println("Insira as notas abaixo")
-            saldoLogado = saldoLogado + valor!!
-            println("Conferindo")
-            extratoDeposito.add(valor.toString())
-            println("voltando ao menu")
-            true
-        } else if (opçao == "3") {
-            println("saques ${extratoSaque}")
-            println("depositos ${extratoDeposito}")
-            println("Saldo Total ${saldoLogado}")
-        } else if (opçao == "4") {
-            break
+
+            "2" -> {
+                println("quanto quer depositar ?")
+                val valor = entradaValor(readLine()?.toDoubleOrNull() ?: 0.0)
+                println("Insira as notas abaixo")
+                saldoLogado += valor
+                println("Conferindo")
+                extratoDeposito.add(valor)
+                println("voltando ao menu")
+            }
+
+            "3" -> {
+                println("saques $extratoSaque")
+                println("depositos $extratoDeposito")
+                println("Saldo Total $saldoLogado")
+            }
+
+            "4" -> break
         }
-
-
     }
+
     println("Obrigado Volte Sempre")
 }
